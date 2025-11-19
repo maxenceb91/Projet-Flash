5d8642fa9a65c42a465f4390cdf6047bc94977bf
Maxence <169144059+maxenceb91@users.noreply.github.com>
2025-11-19 11:39:32 +0100

    fix header

--- projet/partials/header.php
+++ projet/partials/header.php
@@ -1,26 +1,27 @@
 <header>
-<?php
+    <?php
     if (!isset($photo) && (isset($_SESSION['user_id']) || isset($_SESSION['id']))) {
+        require_once __DIR__ . "/../utils/database.php";
         $pdo = connectToDbAndGetPdo();
         $user_id = $_SESSION['user_id'] ?? $_SESSION['id'];
-        
+
         $sql = "SELECT profile_picture FROM user WHERE id = ?";
         $stmt = $pdo->prepare($sql);
         $stmt->execute([$user_id]);
         $user = $stmt->fetch();
-        
-        $photo = !empty($user['profile_picture']) 
+
+        $photo = !empty($user['profile_picture'])
             ? "/Projet-flash/usersfiles/" . $user['profile_picture']
             : "/Projet-flash/assets/img/profil-pp.jpg";
     }
     ?>
-    
+
     <a href="/Projet-flash/index.php">
         <img src="/Projet-flash/assets/img/logo.png" class="logo" alt="logo">
     </a>
     <nav>
-        <a href="/Projet-flash/index.php" class="<?php if ($page == 'accueil') echo 'header-selected'; ?>">Accueil</a>
-        <a href="/Projet-flash/projet/game/memory/score.php" class="<?php if ($page == 'scores') echo 'header-selected'; ?>">Scores</a>
+        <a href="/Projet-flash/index.php" <?php if (isset($page) && $page === 'accueil') echo ' class="header-selected"'; ?>>Accueil</a>
+        <a href="/Projet-flash/projet/game/memory/score.php" <?php if (isset($page) && $page === 'scores') echo ' class="header-selected"'; ?>>Scores</a>
         <a class="contact-btn" href="/Projet-flash/views/contact.php">Nous contacter</a>
         <?php if (isset($_SESSION['user_id']) || isset($_SESSION['id'])): ?>
             <img src="<?php echo $photo; ?>" class="pp-menu">
