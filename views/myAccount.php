aa56085555e28b1d3adba0dd14f041dfbbd8ba2e
Maxence <169144059+maxenceb91@users.noreply.github.com>
2025-11-20 14:20:14 +0100

    Update myAccount.php

--- views/myAccount.php
+++ views/myAccount.php
@@ -103,13 +103,13 @@ function getBestScore($user_id, $difficulty)
 
                 <div class="flex-items">
                     <form method="post">
-                        <label for="pseudo">Nom d'utilisateur</label>
+                        <label for="pseudo"><i class="ri-pencil-ai-line"></i> Nom d'utilisateur</label>
                         <input type="text" id="pseudo" name="pseudo" value="<?php echo $user['pseudo']; ?>" required>
 
-                        <label for="email">Email</label>
+                        <label for="email"><i class="ri-pencil-ai-line"></i> Email</label>
                         <input type="email" id="email" name="email" value="<?php echo $user['email']; ?>" required>
 
-                        <label for="password">Nouveau mot de passe</label>
+                        <label for="password"><i class="ri-pencil-ai-line"></i> Nouveau mot de passe</label>
                         <input type="password" id="password" name="password" placeholder="Laisser vide si pas de changement">
 
                         <button type="submit">Modifier</button>
@@ -118,15 +118,15 @@ function getBestScore($user_id, $difficulty)
                     <div class="best-score">
                         <h1>Meilleurs scores</h1>
                         <div class="score easy">
-                            <span>Facile</span>
+                            <span><i class="ri-emotion-happy-line"></i> Facile</span>
                             <span><?php echo getBestScore($_SESSION["user_id"], 1); ?>s</span>
                         </div>
                         <div class="score medium">
-                            <span>Moyen</span>
+                            <span><i class="ri-emotion-normal-line"></i> Moyen</span>
                             <span><?php echo getBestScore($_SESSION["user_id"], 2); ?>s</span>
                         </div>
                         <div class="score hard">
-                            <span>Difficile</span>
+                            <span><i class="ri-emotion-unhappy-line"></i> Difficile</span>
                             <span><?php echo getBestScore($_SESSION["user_id"], 3); ?>s</span>
                         </div>
                     </div>
