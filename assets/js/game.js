5b68c8dd8b77220796b4fd41c5088e725075f4f7
Maxence <169144059+maxenceb91@users.noreply.github.com>
2025-12-03 10:17:36 +0100

    Update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,46 +2,57 @@ const grid = document.querySelector(".grid");
 const sizeSelect = document.getElementById("size");
 const generateBtn = document.querySelector(".parameters button");
 function generateGrid() {
-  const selectedSize = sizeSelect.value;
-  
-  grid.innerHTML = "";
+    const selectedSize = sizeSelect.value;
 
-  grid.classList.remove("grid-4", "grid-6", "grid-10");
-  
-  let cardCount;
-  let gridClass;
-  
-  switch(selectedSize) {
-    case "4x4":
-      cardCount = 16;
-      gridClass = "grid-4";
-      break;
-    case "6x6":
-      cardCount = 36;
-      gridClass = "grid-6";
-      break;
-    case "10x10":
-      cardCount = 100;
-      gridClass = "grid-10";
-      break;
-    default:
-      cardCount = 16;
-      gridClass = "grid-4";
-  }
-  
-  grid.classList.add(gridClass);
-  
-  for (let i = 0; i < cardCount; i++) {
-    const card = document.createElement("img");
-    card.src = "/Projet-flash/assets/img/memory_card.jpg";
-    card.alt = "A card";
-    card.classList.add("memory-card");
-    grid.appendChild(card);
-  }
-  
-  console.log(`Grille ${selectedSize} générée avec ${cardCount} cartes`);
+    grid.innerHTML = "";
+
+    grid.classList.remove("grid-4", "grid-6", "grid-10");
+
+    let cardCount;
+    let gridClass;
+
+    switch (selectedSize) {
+        case "4x4":
+            cardCount = 16;
+            gridClass = "grid-4";
+            break;
+        case "6x6":
+            cardCount = 36;
+            gridClass = "grid-6";
+            break;
+        case "10x10":
+            cardCount = 100;
+            gridClass = "grid-10";
+            break;
+        default:
+            cardCount = 16;
+            gridClass = "grid-4";
+    }
+
+    grid.classList.add(gridClass);
+
+    for (let i = 0; i < cardCount; i++) {
+        const card = document.createElement("img");
+        card.src = "/Projet-flash/assets/img/memory_card.jpg";
+        card.alt = "A card";
+        card.classList.add("memory-card");
+        grid.appendChild(card);
+    }
+
+    console.log(`Grille ${selectedSize} générée avec ${cardCount} cartes`);
 }
 
-generateBtn.addEventListener("click", generateGrid);
+let time = 0;
+
+generateBtn.addEventListener("click", (event) => {
+    generateGrid()
+    chrono = document.createElement("p")
+    chrono.innerHTML = `<i class="ri-timer-line"></i> Chronomètre: 0`;
+    document.querySelector(".game").appendChild(chrono)
+    setInterval(() => {
+        time++
+        chrono.innerHTML = `<i class="ri-timer-line"></i> Chronomètre: ${time}`;
+    }, 1000);
+});
 
 generateGrid();
